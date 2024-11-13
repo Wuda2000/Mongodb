783f24c493836bd51250bdd28c275433e837fdd8
Wuda2000 <fridawuda@gmail.com>
2024-11-13 13:16:54 +0300

    Update app.py
    This is my python code that connects to the mongodb database. however, in the place of localhost:(nane) , replace with your local host

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 # Connect to MongoDB
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://localhost:(name)/')
 db = client['companyDB']
 
 # Access the collections
